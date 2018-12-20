e0ce1a4bd32d729cd8ec37c6e26c3df04570548f
Rifqi Oktabhiar Erawan <36649810+RifqiErawan@users.noreply.github.com>
2018-12-20 10:46:12 +0700

    Merapikan kode (beautify)

--- src/Account.java
+++ src/Account.java
@@ -3,75 +3,72 @@ import java.util.LinkedList;
 import java.util.Queue; 
 
 public class Account {
-   private int accountNumber; // account number
-   private int pin; // PIN for authentication
-   private double availableBalance; // funds available for withdrawal
-   private double totalBalance; // funds available & pending deposits
-   private String Name;
-   private String BankName;
-   private boolean blocked;
-   public ArrayList<String> History = new ArrayList<>(); //deposit withdrawal
-//   private TrfHistory[] trfHis = new TrfHistory[5]; //transfer
-   Queue<TrfHistory> lastTrans = new LinkedList<>();
+    private int accountNumber; // account number
+    private int pin; // PIN for authentication
+    private double availableBalance; // funds available for withdrawal
+    private double totalBalance; // funds available & pending deposits
+    private String Name;
+    private String BankName;
+    private boolean blocked;
+    public ArrayList<String> History = new ArrayList<>(); //deposit withdrawal
+    //private TrfHistory[] trfHis = new TrfHistory[5]; //transfer
+    Queue<TrfHistory> lastTrans = new LinkedList<>();
    
-   // Account constructor initializes attributes
-   public Account(int theAccountNumber, int thePIN, 
-      double theAvailableBalance, double theTotalBalance, String name, String Bname) {
-      accountNumber = theAccountNumber;
-      pin = thePIN;
-      availableBalance= theAvailableBalance;
-      totalBalance = theTotalBalance;
-      Name = name;
-      BankName = Bname;
-      blocked = false;
-   }
+    // Account constructor initializes attributes
+    public Account(int theAccountNumber, int thePIN, 
+        double theAvailableBalance, double theTotalBalance, String name, String Bname) {
+        accountNumber = theAccountNumber;
+        pin = thePIN;
+        availableBalance = theAvailableBalance;
+        totalBalance = theTotalBalance;
+        Name = name;
+        BankName = Bname;
+        blocked = false;
+    }
 
    // determines whether a user-specified PIN matches PIN in Account
-   public boolean validatePIN(int userPIN) {
-      if (userPIN == pin) {
-         return true;
-      }
-      else {
-         return false;
-      }
-   }
-   /******** M Fajar **************/
-   void SetPIN(int thePIN)
-   {
-       this.pin=thePIN;
-   }
-   // returns available balance
-   public double getAvailableBalance() {
-      return availableBalance;
-   } 
+    public boolean validatePIN(int userPIN) {
+        return (userPIN == pin);
+    }
+    
+    /******** M Fajar **************/
+    void SetPIN(int thePIN) {
+        this.pin = thePIN;
+    }
+    
+    // returns available balance
+    public double getAvailableBalance() {
+        return availableBalance;
+    } 
 
-   // returns the total balance
-   public double getTotalBalance() {
-      return totalBalance;
-   } 
+    // returns the total balance
+    public double getTotalBalance() {
+        return totalBalance;
+    } 
 
-   public void credit(double amount) {
-     totalBalance += amount;
-   }
+    public void credit(double amount) {
+        totalBalance += amount;
+    }
 
-   public void debit(double amount) {
-       totalBalance -= amount;
-       availableBalance -= amount;  
-           
-   }
-   /***** M Fajar **********/
-   public void saving(double amount){
-       totalBalance+=amount;
-       availableBalance+=amount;
-   }
+    public void debit(double amount) {
+        totalBalance -= amount;
+        availableBalance -= amount;  
+    }
+    
+    /***** M Fajar **********/
+    public void saving(double amount){
+        totalBalance += amount;
+        availableBalance += amount;
+    }
    
-   public int getAccountNumber() {
-      return accountNumber;  
-   }
+    public int getAccountNumber() {
+        return accountNumber;  
+    }
 
     public String getName() {
         return Name;
     }
+    
     public String getBankName() {
         return BankName;
     }
@@ -80,26 +77,31 @@ public class Account {
     public boolean isBlocked() {
         return blocked;
     }
+    
     public void setBlocked(boolean blocked) {
         this.blocked = blocked;
     }
+    
     /*********** Daffa *************/
     public void setAvailableBalance(double availableBalance) {
         this.availableBalance = availableBalance;
-    }  
+    } 
+    
     public void addHistory(String his){
        History.add(his);
-   }
+    }
    
-   public void showHistory(){
-          if(History.size() >=1 ){
-       for(int i = 0 ; i<History.size();i++){
-         System.out.println(History.get(i));}
-   }else{
-           System.out.println("There's no history")   ;
-          }
-   }
-   /**** rico ****/
+    public void showHistory(){
+        if(History.size() >= 1 ){
+            for(int i = 0 ; i<History.size(); i++){
+                System.out.println(History.get(i));
+            }
+        } else {
+            System.out.println("There's no history")   ;
+        }
+    }
+    
+    /**** rico ****/
     public Queue<TrfHistory> getLastTrans() {
         return lastTrans;
     }
@@ -107,11 +109,11 @@ public class Account {
     public void setLastTrans(Queue<TrfHistory> lastTrans) {
         this.lastTrans = lastTrans;
     }
+    
     public void show(){
         System.out.println("\n5 Last Transfer History");
         System.out.println("Amount    | To Account");
-        int i=1;
-        
+        int i = 1;
         for(TrfHistory name : lastTrans){
             System.out.println(i+". "+name.getAmount()+" | "+name.getTrfAccount());
             i++;
@@ -119,29 +121,26 @@ public class Account {
     }
     public void showAcc(){
         System.out.println("\n5 Last Account");
-        int i=1;
-        
+        int i = 1;
         for(TrfHistory name : lastTrans){
             System.out.println(i+". "+name.getTrfAccount());
             i++;
         }
     }
     public int search (int sAcc){
-        int i=1;
-        
+        int i = 1;
         for(TrfHistory name : lastTrans){
             if(sAcc==i) return name.getTrfAccount();
             i++;
         }
-        
-       return 0;
+        return 0;
     }
     
-   /***** Entol ****/
+    /***** Entol ****/
     public int getMaxValue() {
         int idMaxValue = 0;
         int id = 1;
-        int i=1;
+        int i = 1;
         
         for(TrfHistory name : lastTrans){
             if(idMaxValue < name.getAmount())  {
@@ -150,14 +149,13 @@ public class Account {
             }
             i++;
         }
-        
         return id;
     }
 
     public int getMinValue() {
-        int idMinValue=0;
+        int idMinValue = 0;
         int id = 1;
-        int i=1;
+        int i = 1;
         
         for(TrfHistory name : lastTrans){
             
@@ -169,9 +167,9 @@ public class Account {
         }
         return id;
     }
+    
     public double getAv(){
-        double Av=0;
-        
+        double Av = 0;
         for(TrfHistory name : lastTrans){
             Av = Av + name.getAmount();
         }
@@ -179,10 +177,9 @@ public class Account {
     }
     
     public TrfHistory searchH (int sAcc){        
-        int i=1;
-        
+        int i = 1;
         for(TrfHistory name : lastTrans){
-            if(sAcc==i) return name;
+            if(sAcc == i) return name;
             i++;
         }
        return null;
